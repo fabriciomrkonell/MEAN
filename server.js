8b66398120780c641a9a202c39f26164e6cbb160
Fabrício Ronchi - Qualix <fabricio_konell@hotmail.com>
2014-06-27 13:46:15 -0300

    Server.js

--- server.js
+++ server.js
@@ -1,36 +1,20 @@
 'use strict';
 
-var express = require('express'),
-		
-		// Novo aplicativo com express	
-		app = express(),
-			
-		// Mongoose para o MongoDB
-		mongoose = require('mongoose'),
+	var express = require('express');				
+	var app = express();		
+	var mongoose = require('mongoose');
+	var port = process.env.PORT || 8080;
+	var database = require('./config/database');
 
-		// Configuração da porta
-		port = process.env.PORT || 8080,
-
-		// Configurações do Banco de Dados
-		database = require('./config/database');
-
-		// Conexão com o MongoDB	
-		mongoose.connect(database.url);
+	// Conexão com o MongoDB	
+	mongoose.connect(database.url);
 	
+	// Configurações
 	app.configure(function() {
-
-		// Diretório
 		app.use(express.static(__dirname + '/public'));
-
-		// Log do console
 		app.use(express.logger('dev'));
-
-		// Informações do POST
 		app.use(express.bodyParser());
-
-		// Simulador de DELETE e PUT
 		app.use(express.methodOverride());
-
 	});
 
 	// Definição das rotas
