8ae4a7162609d4c458ea515e6a0b16d07bc96a3e
Fabrício Ronchi - Qualix <fabricio_konell@hotmail.com>
2014-06-27 11:56:12 -0300

    Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,12 @@
 
 module.exports = function (grunt) {
 
-    // Lê os arquivos do diretório 'tasks'
-    require('grunt-config-dir')(grunt, {
-        configDir: require('path').resolve('tasks')
-    });
+	// Lê os arquivos do diretório 'tasks'
+	require('grunt-config-dir')(grunt, {
+		configDir: require('path').resolve('tasks')
+	});
 
 	// Registra as Tasks
-	grunt.registerTask('build', [ 'uglify' ]);  	
-    
+	grunt.registerTask('build', ['uglify']);
+	
 };
--- tasks/uglify.js
+++ tasks/uglify.js
@@ -1,17 +1,17 @@
 'use strict';
 
 module.exports = function uglify(grunt) {
+  
+  // Plugin de Minificação
+  grunt.loadNpmTasks('grunt-contrib-uglify')
 
-	// Plugin de Minificação
-	grunt.loadNpmTasks('grunt-contrib-uglify')
-
-	// Configuração de Minificação - Destino/Origem
-  	return {
-    	target: {
-      		files: {
-        		'./public/js/app.min.js': ['./public/js/app.js']
-      		}
-   		}
-  	};
+  // Configuração de Minificação - Destino/Origem
+  return {
+    target: {
+      files: {
+        './public/js/app.min.js': ['./public/js/app.js']
+      }
+    }
+  };
 
 };
